fe57a4fc1c9d327cbcfd7353b5638990f25a2c03
Claus Mikkelsen Findinge <clamifi@hotmail.com>
2020-02-20 14:49:16 +0100

    Update Joke.java

--- src/main/java/entities/Joke.java
+++ src/main/java/entities/Joke.java
@@ -16,18 +16,67 @@ public class Joke implements Serializable
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String type;
+    private String joke;
+    private int funnyness;
+    private String description;
 
-    public Joke()
-    {
+    public Joke(){
+        
     }
 
-    public Long getId()
-    {
+    public Joke(Long id, String type, String joke, int funnyness, String description) {
+        this.id = id;
+        this.type = type;
+        this.joke = joke;
+        this.funnyness = funnyness;
+        this.description = description;
+    }
+ 
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(Long id)
-    {
+    public void setId(Long id) {
         this.id = id;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getJoke() {
+        return joke;
+    }
+
+    public void setJoke(String joke) {
+        this.joke = joke;
+    }
+
+    public int getFunnyness() {
+        return funnyness;
+    }
+
+    public void setFunnyness(int funnyness) {
+        this.funnyness = funnyness;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    @Override
+    public String toString() {
+        return "Joke{" + "id=" + id + ", type=" + type + ", joke=" + joke + ", funnyness=" + funnyness + ", description=" + description + '}';
+    }
+  
 }
