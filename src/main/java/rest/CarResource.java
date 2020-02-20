6cf1214c322bff0af2906cd5a98142432ef73abf
MartinBrandstrup <martin.l.brandstrup@gmail.com>
2020-02-20 11:48:48 +0100

    Made initial corrections to CarDTO.java and CarResource.java

--- src/main/java/rest/CarResource.java
+++ src/main/java/rest/CarResource.java
@@ -5,17 +5,11 @@
  */
 package rest;
 
-import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 /**
  *
  * @author Brandstrup
  */
-public class CarResource implements Serializable
+public class CarResource
 {
 
     
