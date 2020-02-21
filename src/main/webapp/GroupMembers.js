9ef6587da4fd21bdc61cbcb3b2204a3833c922db
CaKehr <cph-ck209@cphbusiness.dk>
2020-02-21 16:18:10 +0100

    Added tabel + members
    Needs css

--- src/main/webapp/GroupMembers.js
+++ src/main/webapp/GroupMembers.js
@@ -4,6 +4,23 @@
  * and open the template in the editor.
  */
 
+// use URL with /localhost for testing local
+//var URL = "http://cakehr.dk/2020CA1/api/groupmembers/all"
+var URL = "http://localhost:8080/2020CA1/api/groupmembers/all"
+
+fetch(URL)
+        .then(res => res.json())
+        .then(data =>{
+        let list = data.map(function(member){
+                // table with values
+                return "<tr><td>" + member.name + "</td>" +
+                 "<td>" + member.StudentId + "</td>" +
+                 "<td>" + member.Color + "</td>" +
+                "</tr>";
+            }).join("");
+            document.getElementById("GroupMemberTabel").innerHTML = list;
+})
+
 function addMembersToTable(groupMembers) {
     var tableinfo = groupMembers.map(x => "<tr><td>" + x.name + "</td><td>" + x.studentId + "</td><td>" + x.color + "</td></tr>");
 
